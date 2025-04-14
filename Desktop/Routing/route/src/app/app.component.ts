e2b64f40aee25c3d019e21ba82fc4f823e0d0ccf
kondareddymule <mule.kondareddy@itorizon.com>
2025-04-14 16:10:22 +0530

    Routing

---
+++ Desktop/Routing/route/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterModule, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'route';
+}
---
+++ Desktop/Routing/route/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'contact', component: ContactComponent },
+    {path: "**", component: HomeComponent}
+  ];
